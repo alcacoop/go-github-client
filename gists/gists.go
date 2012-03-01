c2ff8c03f9f9bb813bcac26fb31d0beab3d7fa14
Luca Greco <luca.greco@alcacoop.it>
2012-03-01 14:11:52 +0100

    added first github gists client prototype

---
+++ gists/gists.go
@@ -0,0 +1,45 @@
+package gists
+
+import (
+	ghclient "github.com/alcacoop/go-github-client/client"
+	"net/http"
+)
+
+type Gists struct {
+  *ghclient.GithubClient
+}
+
+func NewGists(ghc *ghclient.GithubClient) *Gists {
+	gists := new(Gists)
+	gists.GithubClient = ghc
+
+	return gists
+}
+
+func (ghc *Gists) GetGistsList() (res *ghclient.GithubResult, err error) {
+	req, err := ghc.NewAPIRequest("GET", "gists", nil)
+
+	if err != nil {
+		return
+	}
+
+	httpc := new(http.Client)
+
+	res, err = ghc.RunRequest(req, httpc)
+
+	return
+}
+
+func (ghc *Gists) GetPublicGistsList() (res *ghclient.GithubResult, err error) {
+	req, err := ghc.NewAPIRequest("GET", "gists/public", nil)
+
+	if err != nil {
+		return
+	}
+
+	httpc := new(http.Client)
+
+	res, err = ghc.RunRequest(req, httpc)
+
+	return
+}
