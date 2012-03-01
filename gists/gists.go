8676f51fd0c38fb4e07de793c0afeda796dc9542
Luca Greco <luca.greco@alcacoop.it>
2012-03-01 14:31:02 +0100

    FEATURE: starred gists, gists by user, gist content

--- gists/gists.go
+++ gists/gists.go
@@ -30,8 +30,50 @@ func (ghc *Gists) GetGistsList() (res *ghclient.GithubResult, err error) {
 	return
 }
 
+func (ghc *Gists) GetStarredGistsList() (res *ghclient.GithubResult, err error) {
+	req, err := ghc.NewAPIRequest("GET", "gists/starred", nil)
+
+	if err != nil {
+		return
+	}
+
+	httpc := new(http.Client)
+
+	res, err = ghc.RunRequest(req, httpc)
+
+	return
+}
+
 func (ghc *Gists) GetPublicGistsList() (res *ghclient.GithubResult, err error) {
 	req, err := ghc.NewAPIRequest("GET", "gists/public", nil)
+
+	if err != nil {
+		return
+	}
+
+	httpc := new(http.Client)
+
+	res, err = ghc.RunRequest(req, httpc)
+
+	return
+}
+
+func (ghc *Gists) GetGistsListByUser(username string) (res *ghclient.GithubResult, err error) {
+	req, err := ghc.NewAPIRequest("GET", "users/"+username+"/gists", nil)
+
+	if err != nil {
+		return
+	}
+
+	httpc := new(http.Client)
+
+	res, err = ghc.RunRequest(req, httpc)
+
+	return
+}
+
+func (ghc *Gists) GetGist(gistId string) (res *ghclient.GithubResult, err error) {
+	req, err := ghc.NewAPIRequest("GET", "gists/"+gistId, nil)
 
 	if err != nil {
 		return
