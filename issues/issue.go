035d61157948fda6199fd475f6dc0a25a0eff14d
Luca Greco <luca.greco@alcacoop.it>
2012-03-07 00:28:53 +0100

    FEAURE: added get/create and update issue

---
+++ issues/issue.go
@@ -0,0 +1,124 @@
+// Copyright 2012 Alca Società Cooperativa. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package issues
+
+import (
+	ghclient "github.com/alcacoop/go-github-client/client"
+	"net/http"
+	"encoding/json"
+	"bytes"
+	"errors"
+)
+
+var (
+	ErrInvalidIssueDataCreate = errors.New("Invalid or nil Create Issue Data")
+	ErrInvalidIssueDataUpdate = errors.New("Invalid or nil Update Issue Data")
+)
+
+// Request the content of a issue from a given issue id.
+// It returns a GithubResult and an error.
+func (ghc *Issues) GetIssue(user, repo, issueId string) (res *ghclient.GithubResult, err error) {
+	req, err := ghc.NewAPIRequest("GET", "repos/"+user+"/"+repo+"/issues/"+
+		issueId, nil)
+
+	if err != nil {
+		return
+	}
+
+	httpc := new(http.Client)
+
+	res, err = ghc.RunRequest(req, httpc)
+
+	return
+}
+
+// TBD
+type IssueDataCreate struct {
+	Title string `json:"title"` // required
+	Body string `json:"body,omitempty"` // optional
+	Assignee string `json:"body,omitempty"` // optional
+	Milestone int `json:"body,omitempty"` // optional
+	Labels []string `json"labels,omitempty"` // optional	
+}
+
+// TBD
+func (idc *IssueDataCreate) IsValid() bool {
+	// TODO: add validate data
+	return true
+}
+
+// TBD
+func (ghc *Issues) CreateIssue(user, repo string, data *IssueDataCreate) (res *ghclient.GithubResult, err error) {
+	if data == nil || !data.IsValid() {
+		err = ErrInvalidIssueDataCreate
+		return
+	}
+
+	body, err := json.Marshal(data)
+	
+	if err != nil {
+		return
+	}
+
+	req, err := ghc.NewAPIRequest("POST", "/repos/"+user+"/"+repo+"/issues", 
+		bytes.NewBuffer(body))
+
+	if err != nil {
+		return
+	}
+
+	httpc := new(http.Client)
+
+	res, err = ghc.RunRequest(req, httpc)
+
+	return	
+}
+
+/////////////////
+// UPDATE ISSUE
+/////////////////
+
+// TBD
+type IssueDataUpdate struct {
+	Title string `json:"title,omitempty"` // optional
+	Body string `json:"body,omitempty"` // optional
+	Assignee string `json:"body,omitempty"` // optional
+	State IssueState `json:"state,omitempty"` // optional
+	Milestone int `json:"body,omitempty"` // optional
+	Labels []string `json"labels,omitempty"` // optional	
+}
+
+// TBD
+func (idc *IssueDataUpdate) IsValid() bool {
+	// TODO: add validate data
+	return true
+}
+
+// TBD
+func (ghc *Issues) UpdateIssue(user, repo, issueId string, data *IssueDataUpdate) (res *ghclient.GithubResult, err error) {
+	if data == nil || !data.IsValid() {
+		err = ErrInvalidIssueDataUpdate
+		return
+	}
+
+	body, err := json.Marshal(data)
+	
+	if err != nil {
+		return
+	}
+
+	req, err := ghc.NewAPIRequest("PATCH", "/repos/"+user+"/"+repo+"/issues"+issueId, 
+		bytes.NewBuffer(body))
+
+	if err != nil {
+		return
+	}
+
+	httpc := new(http.Client)
+
+	res, err = ghc.RunRequest(req, httpc)
+
+	return	
+}
